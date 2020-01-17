09df6862a8392252c757c04c9414ae5de369796b
Benoit Jaunin <jaunin.b@fr.ibm.com>
2020-01-17 19:30:01 +0100

    Create the intermediary pipeline file

--- toolchain-to-template.sh
+++ toolchain-to-template.sh
@@ -34,12 +34,8 @@ function download_classic_pipeline() {
   echo "about to do: curl -H Accept: application/x-yaml ${PIPELINE_API_URL}"
   curl -H "Authorization: $BEARER_TOKEN" -H "Accept: application/x-yaml"  -o "${SOURCE_PIPELINE_ID}.yaml" "${PIPELINE_API_URL}"
 
-  # echo "YAML from source pipeline"
+  # echo "YAML from source classic pipeline"
   # cat "${SOURCE_PIPELINE_ID}.yaml"
-
-  # Find the token url for the git tile
-  # echo "about to get: ${PIPELINE_API_URL}/pipelines/${SOURCE_PIPELINE_ID}/inputsources"
-  # curl -H "Authorization: $BEARER_TOKEN" -H "Content-Type: application/json" -o "${SOURCE_PIPELINE_ID}_inputsources.json" "${PIPELINE_API_URL}/pipelines/${SOURCE_PIPELINE_ID}/inputsources"
 
   # convert the yaml to json 
   # yq r -j ${SOURCE_PIPELINE_ID}.yaml | tee ${SOURCE_PIPELINE_ID}.json
@@ -53,9 +49,6 @@ function download_classic_pipeline() {
   jq -r '.stages[] | select( .inputs and .inputs[0].type=="git") | .inputs[0].url' $SOURCE_PIPELINE_ID.json |\
   while IFS=$'\n\r' read -r input_gitrepo 
   do
-    # token_url=$(cat "${SOURCE_PIPELINE_ID}_inputsources.json" | jq -r --arg git_repo "$input_gitrepo" '.[] | select( .repo_url==$git_repo ) | .token_url')
-    # echo "token url: $input_gitrepo => $token_url"
-
     # Add a token field/line for input of type git and url being $git_repo
     cp -f $TARGET_PIPELINE_ID.json tmp-$TARGET_PIPELINE_ID.json
 
@@ -105,15 +98,6 @@ function download_classic_pipeline() {
   # cat "$TARGET_PIPELINE_ID.yml"
   # echo "==="
 
-  # Include the yaml as rawcontent (ie needs to replace cr by \n and " by \" )
-  # echo '{}' | jq --rawfile yaml $TARGET_PIPELINE_ID.yaml '{"config": {"format": "yaml","content": $yaml}}' > ${TARGET_PIPELINE_ID}_configuration.json
-
-  # # HTTP PUT to target pipeline
-  # curl -is -H "Authorization: $BEARER_TOKEN" -H "Content-Type: application/json" -X PUT -d @${TARGET_PIPELINE_ID}_configuration.json $PIPELINE_API_URL/pipelines/$TARGET_PIPELINE_ID/configuration 
-
-  # Check the configuration if it has been applied correctly
-  # curl -H "Authorization: $BEARER_TOKEN" -H "Accept: application/json" $PIPELINE_API_URL/pipelines/$TARGET_PIPELINE_ID/configuration
-
   # echoing the secured properties (pipeline and stage) that can not be valued there
   echo "The following pipeline secure properties needs to be updated with appropriate values:"
   jq -r '.properties[] | select(.type=="secure") | .name' ${TARGET_PIPELINE_ID}.json
@@ -133,7 +117,28 @@ function download_tekton_pipeline() {
   echo "about to do: curl -H Accept: application/x-yaml ${PIPELINE_API_URL}"
   curl -H "Authorization: $BEARER_TOKEN" -H "Accept: application/x-yaml"  -o "${SOURCE_PIPELINE_ID}.yaml" "${PIPELINE_API_URL}"
 
+  echo "YAML from source tekton pipeline"
+  echo "==="
   cat "${SOURCE_PIPELINE_ID}.yaml"
+  echo "==="
+
+  # convert the yaml to json 
+  # yq r -j ${SOURCE_PIPELINE_ID}.yaml | tee ${SOURCE_PIPELINE_ID}.json
+  yq r -j ${SOURCE_PIPELINE_ID}.yaml > ${SOURCE_PIPELINE_ID}.json
+
+  #######
+  cp ${SOURCE_PIPELINE_ID}.json ${TARGET_PIPELINE_ID}.json
+  # Work here !
+  # Rename envProperties to properties
+  # For each properties, make the type lowercase
+  #######
+
+  yq r $TARGET_PIPELINE_ID.json > $TARGET_PIPELINE_ID.yml
+
+  echo "$TARGET_PIPELINE_ID.yml generated"
+  echo "==="
+  cat "$TARGET_PIPELINE_ID.yml"
+  echo "==="
 }
 
 #### MAIN ####
@@ -312,6 +317,9 @@ do
           TARGET_PIPELINE_ID="pipeline_${SERVICE_NAME}"
 
           download_tekton_pipeline "${PIPELINE_EXTERNAL_API_URL}" "${SERVICE_INSTANCE_ID}" "${TARGET_PIPELINE_ID}" "${REPO_DETAILS}"
+
+          PIPELINE_FILE_NAME="${TARGET_PIPELINE_ID}.yml"
+          PIPELINE_FILE_NAMES="${PIPELINE_FILE_NAMES},${PIPELINE_FILE_NAME}"
 
         else
           echo "WARNING, unknown pipeline type $PIPELINE_TYPE for instance $SERVICE_INSTANCE_ID"
